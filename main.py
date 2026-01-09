6e2fe500f1fc7d79ebcafc0fb84e4ad234ff2ebe
jojo2504 <jojo.duchuy@gmail.com>
2026-01-09 19:38:49 +0100

    moved entry point

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from collect import Collector
-from extract import Extractor
-from utils import to_json_url
+from dataframe import build_dataframe
+from enrich import Enrichment
 
+if __name__ == "__main__":
+    enrichment = Enrichment()
+    rss = "https://www.cert.ssi.gouv.fr/avis/feed/"
+    df = build_dataframe(rss, "Avis", enrichment)
+    df.to_csv("cves_consolidees.csv", index=False)
+    print("CSV généré : cves_consolidees.csv")
